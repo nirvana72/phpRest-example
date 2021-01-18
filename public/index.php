c1be35e6664b4be2d4306b62b202848e9c73fe2b
nirvana72 <15279663@qq.com>
2021-01-18 18:42:02 +0800

    swagger 测试

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use PhpRest\Application;
+use PhpRest\Utils\EnvHelper as Env;
 
 require __DIR__.'/../vendor/autoload.php';
 require '/workspace/phpRest/vendor/autoload.php';
@@ -11,13 +12,29 @@ $app = Application::createDefault(__DIR__.'/../config/config.php');
 // $app->get('\PhpRest\Test\ControllerBuildTest')->test1(); exit;
 // $app->get('\PhpRest\Test\EntityBuildTest')->test2(); exit;
 
-// 加载路由
-$app->loadRoutesFromPath( __DIR__.'/../App/Controller', 'App\\Controller');
-
-// 加载全局Hook
-$app->addGlobalHooks([
-    \PhpRest\Hook\CorsHook::class
+// 跨域支持
+$app->addGlobalHooks([ 
+    \PhpRest\Hook\CorsHook::class 
 ]);
 
-// 执行请求
+// 加载路由
+$app->loadRoutesFromPath( __DIR__.'/../App/Controller', 'App\Controller');
+
+// swagger
+if (Env::get('app.env') !== 'production') {
+    \PhpRest\Swagger\SwaggerHandler::register(
+        $app, 
+        [
+            'demo' => 'App\Controller'
+        ], 
+        function($swagger, $group) {
+            $api_key['type'] = 'apiKey';
+            $api_key['in'] = 'header';
+            $api_key['name'] = Env::get('jwt.name');
+            $swagger->securityDefinitions['token'] = $api_key;
+        }
+    );
+}
+
+// 解析请求
 $app->dispatch();
