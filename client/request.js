1dcbd7dc3edab41b13acddf685dc306afaafe017
nirvana72 <15279663@qq.com>
2021-01-22 14:37:09 +0800

    增加一个简易测试客户端

---
+++ client/request.js
@@ -0,0 +1,23 @@
+const request = axios.create({
+  baseURL: 'http://www.phpRest.local',
+  timeout: 60000
+})
+
+request.interceptors.request.use(
+  config => {
+    config.headers['Authorization'] = '123123'
+    return config
+  },
+  error => {
+    console.log(error)
+  }
+)
+
+request.interceptors.response.use(
+  response => {
+    console.log(response.data)
+  },
+  error => { // 非200状态
+    console.log(error)
+  }
+)
